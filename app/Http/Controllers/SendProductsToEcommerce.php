8ef309afafda721835f2a33c74b2b173bc23a179
Antonio Tomas <adminportales@promolife.com.mx>
2022-03-10 10:27:39 -0600

    Conexion y carga de productos a woocommerce

--- app/Http/Controllers/SendProductsToEcommerce.php
+++ app/Http/Controllers/SendProductsToEcommerce.php
@@ -2,16 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use Automattic\WooCommerce\Client as WooCommerceClient;
+
 use App\Models\Product;
 use Illuminate\Http\Request;
+
 
 class SendProductsToEcommerce extends Controller
 {
     public function setAllProducts()
     {
-        $products = Product::where('ecommerce', 1)->get();
 
-        return $products;
+
+        $woocommerce = new WooCommerceClient(
+            env('STORE_URL', ''), // Your store URL
+            env('CONSUMER_KEY', ''), // Your consumer key
+            env('CONSUMER_SECRET', ''), // Your consumer secret
+            [
+                'wp_api' => true, // Enable the WP REST API integration
+                'version' => 'wc/v3' // WooCommerce WP REST API version
+            ]
+        );
+        $products = Product::where('ecommerce', 1)->get();
+        $data = ['create' => []];
+        foreach ($products as $product) {
+            $dataProduct = [
+                'name' => $product->name,
+                'type' => 'simple',
+                'regular_price' => $product->price * 1.76,
+                'categories' => [
+                    [
+                        'id' => 59
+                    ]
+                ],
+                'images' => [
+                    [
+                        'src' => $product->image
+                    ]
+                ]
+            ];
+            array_push($data['create'], $dataProduct);
+        }
+
+        echo '<pre>';
+        print_r($woocommerce->post('products/batch', $data));
+        echo '</pre>';
     }
     public function updateAllProducts()
     {
