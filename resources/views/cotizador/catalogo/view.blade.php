4fdc4b158923770deb95df2fddbe4d7a9a9305dc
Antonio Tomas <antoniotd87@gmail.com>
2022-09-23 15:10:15 -0500

    Responsive

--- resources/views/cotizador/catalogo/view.blade.php
+++ resources/views/cotizador/catalogo/view.blade.php
@@ -1,79 +1,84 @@
 @section('title', __('Products'))
 <div class="container-fluid ">
-    <div class="btn-group">
-        <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
-            aria-expanded="false">
-            Right-aligned menu
-        </button>
-        <div class="dropdown-menu dropdown-menu-right" wire:self>
-            <div class="shadow p-3">
-                <p>Filtros de busqueda</p>
-                <input wire:model='nombre' type="text" class="form-control mb-2" name="search" id="search"
-                    placeholder="Nombre">
-                @permission('ver-sku-s-de-proveedores')
-                    <input wire:model='sku' type="text" class="form-control mb-2" name="search" id="search"
-                        placeholder="SKU">
-                @endpermission
-                <input wire:model='color' type="text" class="form-control mb-2" name="color" id="color"
-                    placeholder="Ingrese el color">
-                <input wire:model='category' type="text" class="form-control mb-2" name="category" id="category"
-                    placeholder="Ingrese la familia">
-                @permission('ver-proveedores')
-                    <select wire:model='proveedor' name="proveedores" id="provee" class="form-control mb-2">
-                        <option value="">Seleccione Proveedor...</option>
-                        @foreach ($proveedores as $provider)
-                            <option value="{{ $provider->id }}">{{ $provider->company }}</option>
-                        @endforeach
-                    </select>
-                @endpermission
-                {{-- <select wire:model='color' name="colores" id="provee" class="form-control mb-2">
+    <div class="row mb-2 d-md-none">
+        <input type="text" class="form-control" placeholder="Buscar Productos">
+    </div>
+    <div class="row pb-2 d-sm-none">
+        <div class="btn-group  w-100">
+            <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
+                aria-expanded="false">
+                + Filtros
+            </button>
+            <div class="dropdown-menu dropdown-menu-right w-100 shadow" wire:self>
+                <div class="p-3">
+                    <p>Filtros de busqueda</p>
+                    <input wire:model='nombre' type="text" class="form-control mb-2" name="search" id="search"
+                        placeholder="Nombre">
+                    @permission('ver-sku-s-de-proveedores')
+                        <input wire:model='sku' type="text" class="form-control mb-2" name="search" id="search"
+                            placeholder="SKU">
+                    @endpermission
+                    <input wire:model='color' type="text" class="form-control mb-2" name="color" id="color"
+                        placeholder="Ingrese el color">
+                    <input wire:model='category' type="text" class="form-control mb-2" name="category" id="category"
+                        placeholder="Ingrese la familia">
+                    @permission('ver-proveedores')
+                        <select wire:model='proveedor' name="proveedores" id="provee" class="form-control mb-2">
+                            <option value="">Seleccione Proveedor...</option>
+                            @foreach ($proveedores as $provider)
+                                <option value="{{ $provider->id }}">{{ $provider->company }}</option>
+                            @endforeach
+                        </select>
+                    @endpermission
+                    {{-- <select wire:model='color' name="colores" id="provee" class="form-control mb-2">
                 <option value="">Seleccione color...</option>
                 @foreach ($colores as $color)
                     <option value="{{ $color->id }}">{{ $color->color }}</option>
                 @endforeach
                 </select> --}}
-                <select wire:model='type' name="types" id="type" class="form-control mb-2">
-                    <option value="">Importacion o Catalogo...</option>
-                    @foreach ($types as $type)
-                        <option value="{{ $type->id }}">{{ $type->type }}</option>
-                    @endforeach
-                </select>
-                <p class="mb-0">Precio</p>
-                <div class="d-flex align-items-center mb-2">
-                    <input wire:model='precioMin' type="number" class="form-control" name="search" id="search"
-                        placeholder="Precio Minimo" min="0" value="0">
-                    -
-                    <input wire:model='precioMax' type="number" class="form-control" name="search" id="search"
-                        placeholder="Precio Maximo" value="{{ $price }}" max="{{ $price }}">
-                </div>
-                @permission('ver-stock')
-                    <p class="mb-0">Stock</p>
+                    <select wire:model='type' name="types" id="type" class="form-control mb-2">
+                        <option value="">Importacion o Catalogo...</option>
+                        @foreach ($types as $type)
+                            <option value="{{ $type->id }}">{{ $type->type }}</option>
+                        @endforeach
+                    </select>
+                    <p class="mb-0">Precio</p>
                     <div class="d-flex align-items-center mb-2">
-                        <input wire:model='stockMin' type="number" class="form-control" placeholder="Stock Minimo"
-                            min="0" value="0">
+                        <input wire:model='precioMin' type="number" class="form-control" name="search" id="search"
+                            placeholder="Precio Minimo" min="0" value="0">
                         -
-                        <input wire:model='stockMax' type="number" class="form-control" placeholder="Stock Maximo"
-                            value="{{ $stock }}" max="{{ $stock }}">
+                        <input wire:model='precioMax' type="number" class="form-control" name="search" id="search"
+                            placeholder="Precio Maximo" value="{{ $price }}" max="{{ $price }}">
                     </div>
-                    <p class="mb-0">Ordenar por Stock</p>
-                    <select wire:model='orderStock' name="orderStock" id="provee" class="form-control mb-2">
+                    @permission('ver-stock')
+                        <p class="mb-0">Stock</p>
+                        <div class="d-flex align-items-center mb-2">
+                            <input wire:model='stockMin' type="number" class="form-control" placeholder="Stock Minimo"
+                                min="0" value="0">
+                            -
+                            <input wire:model='stockMax' type="number" class="form-control" placeholder="Stock Maximo"
+                                value="{{ $stock }}" max="{{ $stock }}">
+                        </div>
+                        <p class="mb-0">Ordenar por Stock</p>
+                        <select wire:model='orderStock' name="orderStock" id="provee" class="form-control mb-2">
+                            <option value="">Ninguno</option>
+                            <option value="ASC">De menor a mayor</option>
+                            <option value="DESC">De mayor a menor</option>
+                        </select>
+                    @endpermission
+                    <p class="mb-0">Ordenar por Precio</p>
+                    <select wire:model='orderPrice' name="orderPrice" id="provee" class="form-control mb-2">
                         <option value="">Ninguno</option>
                         <option value="ASC">De menor a mayor</option>
                         <option value="DESC">De mayor a menor</option>
                     </select>
-                @endpermission
-                <p class="mb-0">Ordenar por Precio</p>
-                <select wire:model='orderPrice' name="orderPrice" id="provee" class="form-control mb-2">
-                    <option value="">Ninguno</option>
-                    <option value="ASC">De menor a mayor</option>
-                    <option value="DESC">De mayor a menor</option>
-                </select>
-                <button class="btn btn-primary btn-block" wire:click="limpiar">Limpiar Filtros</button>
+                    <button class="btn btn-primary btn-block" wire:click="limpiar">Limpiar Filtros</button>
+                </div>
             </div>
         </div>
     </div>
     <div class="row">
-        <div class="col-lg-2 col-md-4 col-sm-5 d-none d-sm-block">
+        <div class="col-lg-2 col-md-4 col-sm-5  d-none d-sm-block">
             <div class="shadow p-3">
                 <p>Filtros de busqueda</p>
                 <input wire:model='nombre' type="text" class="form-control mb-2" name="search" id="search"
@@ -163,7 +168,7 @@
                 @endif
                 @foreach ($products as $row)
                     {{-- {{ $row->firstImage->image_url }} --}}
-                    <div class="col-md-4 col-lg-3 col-sm-6 d-none d-sm-flex justify-content-center">
+                    <div class="col-md-4 col-lg-3 col-sm-6  d-none d-sm-flex justify-content-center">
                         <div class="card mb-4" style="width: 14rem;">
                             <div class="card-body text-center shadow-sm">
                                 @php
@@ -198,7 +203,8 @@
                         </div>
                     </div>
                     <div class="d-block d-sm-none" style="width: 100%;">
-                        <div class="card mb-4" style="width: 100%;">
+                        <div class="card mb-2" style="width: 100%;" data-toggle="modal" data-target="#modalProduct"
+                            wire:click="showProduct({{ $row->id }})">
                             <div class="card-body text-center shadow-sm d-flex">
                                 @php
                                     $priceProduct = $row->price;
@@ -208,38 +214,42 @@
                                         $priceProduct = round($priceProduct - $priceProduct * ($row->provider->discount / 100), 2);
                                     }
                                 @endphp
-                                <div>
+                                <div style="width: 35%">
                                     <img src="{{ $row->firstImage ? $row->firstImage->image_url : '' }}"
-                                        class="card-img-top " alt="{{ $row->name }}"
-                                        style="max-width: 100%; max-height: 150px; width: auto">
+                                        alt="{{ $row->name }}"
+                                        style="max-width: 100px; max-height: 100px; height: 100px; width: auto">
                                 </div>
-
-                                <div>
-                                    <h5 class="card-title" style="text-transform: capitalize">{{ $row->name }}
+                                <div class="text-left">
+                                    <h5 class="card-title p-0" style="text-transform: capitalize">
+                                        {{ $row->name }}
                                     </h5>
+                                    <p class=" m-0 pt-0">{{ Str::limit($row->description, 25, '...') }}</p>
                                     @permission('ver-sku-s-de-proveedores')
-                                        <p class=" m-0 pt-1"><strong>SKU:</strong> {{ $row->sku }}</p>
+                                        <p class="m-0 pt-0"><strong>SKU:</strong> {{ $row->sku }}</p>
                                     @endpermission
+
                                     <div class="d-flex justify-content-between">
                                         @permission('ver-stock')
-                                            <p class=" m-0 pt-1">Stock: {{ $row->stock }}</p>
+                                            <p class=" m-0 pt-0">Stock: {{ $row->stock }}</p>
                                         @endpermission
-                                        <p class=" m-0 pt-1">$
+                                        <p class=" m-0 pt-0">Precio: $
                                             {{ round($priceProduct / ((100 - $utilidad) / 100), 2) }}</p>
                                     </div>
-                                    <br>
-                                    <button type="button" class="btn btn-primary" data-toggle="modal"
-                                        data-target="#modalProduct" wire:click="showProduct({{ $row->id }})">
-                                        Vista Rapida
-                                    </button>
                                 </div>
+                                {{-- <div class="al">
+                                        <button type="button" class="btn btn-primary" data-toggle="modal"
+                                            data-target="#modalProduct"
+                                            wire:click="showProduct({{ $row->id }})">
+                                            Ver
+                                        </button>
+                                    </div> --}}
                             </div>
                         </div>
                     </div>
                 @endforeach
             </div>
             <div class="d-flex justify-content-center">
-                {{ $products->links() }}
+                {{ $products->onEachSide(1)->links() }}
             </div>
             @livewire('product')
         </div>
--- resources/views/cotizador/components/navbar.blade.php
+++ resources/views/cotizador/components/navbar.blade.php
@@ -1,51 +1,55 @@
-<nav class="navbar navbar-expand topbar mb-4 static-top shadow h-auto">
-    <ul class="navbar-nav mr-auto">
-        <!-- Nav Item - User Information -->
-        <li class="nav-item no-arrow">
-            <div class="nav-link text-light d-flex align-items-center">
-                <img class="imagen img-fluid h-100" src="{{ asset('/img/logoOnly.png') }}" alt="bhtrade"></a>
-                <div class="text-center">
-                    <h6 class="m-0 text-white font-weight-bold" style="font-family:'Myriad Pro Bold';font-weight:bold;">PROMO CONNECT</h6>
-                    <p class="m-0 text-white" style="font-family:'Myriad Pro Regular';font-weight:normal;">Encuentra tus articulos</p>
-                </div>
+<nav class="navbar navbar-expand-md topbar mb-2 static-top shadow h-auto">
+    <a href="/catalogo" class="navbar-brand">
+        <div class="text-light d-flex align-items-center">
+            <img class="imagen" height="60" src="{{ asset('/img/logoOnly.png') }}" alt="bhtrade">
+            <div>
+                <h6 class="m-0 text-white font-weight-bold" style="font-family:'Myriad Pro Bold';font-weight:bold;">PROMO
+                    CONNECT
+                </h6>
+                <p class="m-0 text-white d-none d-sm-block" style="font-family:'Myriad Pro Regular';font-weight:normal;">Encuentra
+                    tus articulos</p>
             </div>
-        </li>
-    </ul>
-    <!-- Topbar Navbar -->
-    <ul class="navbar-nav ml-auto">
-        <li class="nav-item d-flex align-items-center">
-            <input type="text" class="form-control">
-        </li>
-        <div class="topbar-divider d-none d-sm-block"></div>
-
-        <!-- Nav Item - User Information -->
-        @auth
-            <li class="nav-item dropdown no-arrow">
-                <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
-                    aria-haspopup="true" aria-expanded="false">
-                    <span class="mr-2 d-none d-lg-inline text-white"> {{ Auth::user()->name }}</span>
-                    <img class="img-profile rounded-circle" src="img/undraw_profile.svg">
-                </a>
-
-                <!-- Dropdown - User Information -->
-                <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                    @role('admin')
-                        <a class="dropdown-item" href="{{ url('admin/') }}">
-                            <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                            Administrador
-                        </a>
-                    @endrole
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                        <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                        Salir
-                    </a>
-                </div>
+        </div>
+    </a>
+    <button class="navbar-toggler text-light" type="button" data-toggle="collapse" data-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <!-- Topbar Navbar -->
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item d-md-flex d-none align-items-center">
+                <input type="text" class="form-control">
             </li>
-        @endauth
-    </ul>
-</nav>
+            <div class="topbar-divider d-none d-md-block"></div>
+            <!-- Nav Item - User Information -->
+            @auth
+                <li class="nav-item dropdown no-arrow">
+                    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <span class="mr-2 d-inline text-white"> {{ Auth::user()->name }}</span>
+                        <img class="img-profile rounded-circle" src="img/undraw_profile.svg">
+                    </a>
 
+                    <!-- Dropdown - User Information -->
+                    <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                        @role('admin')
+                            <a class="dropdown-item" href="{{ url('admin/') }}">
+                                <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+                                Administrador
+                            </a>
+                        @endrole
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                            <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Salir
+                        </a>
+                    </div>
+                </li>
+            @endauth
+        </ul>
+    </div>
+</nav>
 
 <!-- Logout Modal-->
 <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
