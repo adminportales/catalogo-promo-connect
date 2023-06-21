a440c37a8b2b5e45ea6f6c75e6d027a82ca82742
Antonio <adminportales@promolife.com.mx>
2023-06-21 16:58:08 -0600

    Actualizacion del WS de promo opcion

--- app/Http/Controllers/Providers/PromoOpcionController.php
+++ app/Http/Controllers/Providers/PromoOpcionController.php
@@ -16,37 +16,36 @@ class PromoOpcionController extends Controller
     public function getAllProductsPromoOption()
     {
         $user = "DFE4516";
-        $xapikey = "ad3bdbcfd679bf6fd0b97b4b13809b22";
-        $headers = array(
-            "user: " . $user,
-            "x-api-key: " . $xapikey,
-        );
+        $passowrd = "5MrZtuzmiiuwSswLuONi";
+        $postFields = [
+            'user' => $user,
+            'password' => $passowrd,
+        ];
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_POST, 1);
-        // curl_setopt($ch, CURLOPT_POSTFIELDS, "demo=1"); //Opcional
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields); //Opcional
         curl_setopt(
             $ch,
             CURLOPT_URL,
-            "https://www.contenidopromo.com/wsds/mx/catalogo/"
+            "https://promocionalesenlinea.net/api/all-products"
         );
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         $result = curl_exec($ch);
         curl_close($ch);
-        // Convertir en array
         $result = json_decode($result, true);
-        if (isset($result['error'])) {
-            FailedJobsCron::create([
-                'name' => 'Promo Opcion',
-                'message' => $result['error'],
-                'status' => 0,
-                'type' =>   1
-            ]);
-            return;
+        if (!isset($result['success'])) {
+            return $result;
         }
-        // if (!$result['error']) {
+        if (!$result['success'] == true) {
+            return $result;
+        }
+
+        if (!isset($result['response'])) {
+            return $result;
+        }
+        $productsWs =  $result['response'];
         $maxSKU = Product::max('internal_sku');
         $idSku = null;
         if (!$maxSKU) {
@@ -55,14 +54,23 @@ class PromoOpcionController extends Controller
             $idSku = (int) explode('-', $maxSKU)[1];
             $idSku++;
         }
-        foreach ($result as $product) {
+
+        foreach ($productsWs as $product) {
+            // Verificar si la categoria existe y si no registrarla
+            $categoria = null;
+            $slug = mb_strtolower(str_replace(' ', '-', $product['categorias']));
+            $categoria = Category::where("slug", $slug)->first();
+            if (!$categoria) {
+                $categoria = Category::create([
+                    'family' => ucfirst($product['categorias']), 'slug' => $slug,
+                ]);
+            }
+            // Verificar si la subcategoria existe y si no registrarla
+            $subcategoria = Subcategory::find(1);
+
             $data = [
-                'internal_sku' => "PROM-" . str_pad($idSku, 7, "0", STR_PAD_LEFT),
-                'sku' => $product['item_code'],
-                'sku_parent' => $product['parent_code'],
-                'name' => $product['name'],
-                'price' =>  0,
-                'description' => $product['description'],
+                'sku_parent' => $product['skuPadre'],
+                'description' => $product['descripcion'],
                 'stock' => 0,
                 'producto_promocion' => false,
                 'producto_nuevo' => false,
@@ -70,79 +78,99 @@ class PromoOpcionController extends Controller
                 'type_id' => 1,
                 'provider_id' => 2,
             ];
-            $color = null;
-            $slug = mb_strtolower(str_replace(' ', '-', $product['color']));
-            $color = Color::where("slug", $slug)->first();
-            if (!$color) {
-                $color = Color::create([
-                    'color' => ucfirst($product['color']), 'slug' => $slug,
-                ]);
-            }
-            $data['color_id'] = $color->id;
-            // Verificar si la categoria existe y si no registrarla
-            $categoria = null;
-            $slug = mb_strtolower(str_replace(' ', '-', $product['family']));
-            $categoria = Category::where("slug", $slug)->first();
-            if (!$categoria) {
-                $categoria = Category::create([
-                    'family' => ucfirst($product['family']), 'slug' => $slug,
-                ]);
-            }
-            $attributes = [
-                [
-                    'attribute' => 'Tamaño',
-                    'slug' => 'size',
-                    'value' => $product['size'],
-                ],
-                [
-                    'attribute' => 'Material',
-                    'slug' => 'material',
-                    'value' => $product['material'],
-                ],
-                [
-                    'attribute' => 'Capacidad',
-                    'slug' => 'capacity',
-                    'value' => $product['capacity'],
-                ],
-                [
-                    'attribute' => 'Impresion',
-                    'slug' => 'printing',
-                    'value' => $product['printing'],
-                ],
-                [
-                    'attribute' => 'Area de impresion',
-                    'slug' => 'printing_area',
-                    'value' => $product['printing_area'],
-                ],
-            ];
-
-            // Verificar si la subcategoria existe y si no registrarla
-            $subcategoria = Subcategory::find(1);
-
-            $productExist = Product::where('sku', $product['item_code'])->first();
-            if (!$productExist) {
-                $newProduct = Product::create($data);
-                $newProduct->images()->create([
-                    'image_url' => $product['img']
-                ]);
-                $newProduct->productCategories()->create([
-                    'category_id' => $categoria->id,
-                    'subcategory_id' => $subcategoria->id,
-                ]);
-                foreach ($attributes as $attr) {
-                    $newProduct->productAttributes()->create($attr);
+            foreach ($product['hijos'] as $productHijo) {
+                $color = null;
+                if ($productHijo['color'] == null) {
+                    $color = Color::find(1);
+                } else {
+                    $slug = mb_strtolower(str_replace(' ', '-', $productHijo['color']));
+                    $color = Color::where("slug", $slug)->first();
+                    if (!$color) {
+                        $color = Color::create([
+                            'color' => ucfirst($productHijo['color']), 'slug' => $slug,
+                        ]);
+                    }
                 }
-                $idSku++;
-                // dd($newProduct);
+                $data['color_id'] = $color->id;
+                $attributes = [
+                    [
+                        'attribute' => 'Tipo',
+                        'slug' => 'type',
+                        'value' => $productHijo['tipo'],
+                    ],
+                    [
+                        'attribute' => 'Talla',
+                        'slug' => 'size',
+                        'value' => $productHijo['talla'],
+                    ],
+                    [
+                        'attribute' => 'Material',
+                        'slug' => 'material',
+                        'value' => $product['material'],
+                    ],
+                    [
+                        'attribute' => 'Capacidad',
+                        'slug' => 'capacity',
+                        'value' => $product['capacidad'],
+                    ],
+                    // Medidas
+                    [
+                        'attribute' => 'Medidas',
+                        'slug' => 'medidas',
+                        'value' => $product['medidas'],
+                    ],
+                    [
+                        'attribute' => 'Impresion',
+                        'slug' => 'printing',
+                        'value' => $product['impresion']['tecnicaImpresion'],
+                    ],
+                    [
+                        'attribute' => 'Area de impresion',
+                        'slug' => 'printing_area',
+                        'value' => $product['impresion']['areaImpresion'],
+                    ],
+                ];
+                $data['price'] =  $productHijo['precio'];
+                $data['name'] = $productHijo['nombreHijo'];
+                $data['sku'] = $productHijo['skuHijo'];
+                $data['internal_sku'] = "PROM-" . str_pad($idSku, 7, "0", STR_PAD_LEFT);
+                $productExist = Product::where('sku', $productHijo['skuHijo'])->first();
+                if (!$productExist) {
+                    if ($productHijo['estatus'] == 0 || $productHijo['estatus'] == '') {
+                        // Romper aqui y continuar con el siguiente ciclo del foreach
+                        continue;
+                    }
+                    $newProduct = Product::create($data);
+                    $imagenes =  count($productHijo['imagenesHijo']) <= 0 ? $product['imagenesPadre'] : $productHijo['imagenesHijo'];
+                    foreach ($imagenes as $image) {
+                        $newProduct->images()->create([
+                            'image_url' => $image
+                        ]);
+                    }
+                    $newProduct->productCategories()->create([
+                        'category_id' => $categoria->id,
+                        'subcategory_id' => $subcategoria->id,
+                    ]);
+                    foreach ($attributes as $attr) {
+                        $newProduct->productAttributes()->create($attr);
+                    }
+                    $idSku++;
+                } else {
+                    // Actualizar el precio
+                    $productExist->price = $productHijo['precio'];
+                    $productExist->save();
+                }
             }
         }
 
-        $allProducts = Product::where('provider_id', 3)->where('visible', 1)->get();
+        $allProducts = Product::where('provider_id', 2)->where('visible', 1)->get();
         foreach ($allProducts as $key => $value) {
-            foreach ($result as $product) {
-                if ($value->sku == $product['item_code']) {
-                    unset($allProducts[$key]);
-                    break;
+            foreach ($productsWs as $product) {
+                foreach ($product['hijos'] as $productHijo) {
+                    if ($value->sku == $productHijo['skuHijo'] && $productHijo['estatus'] == '1') {
+                        unset($allProducts[$key]);
+                        break;
+                    }
                 }
             }
         }
@@ -157,81 +185,53 @@ class PromoOpcionController extends Controller
         return $result;
     }
 
-    public function getPricePromoOpcion()
-    {
-        $client = new \nusoap_client('http://desktop.promoopcion.com:8095/wsFullFilmentMXP/FullFilmentMXP.asmx?wsdl', 'wsdl');
-        $err = $client->getError();
-        if ($err) {
-            echo 'Error en Constructor' . $err;
-        }
-        $CardCode = "DFE4516";
-        $pass = 'DIS00048';
-
-        $products = Product::where('provider_id', 2)->get();
-        $errors = [];
-        foreach ($products as $product) {
-            $param = array('CardCode' => $CardCode, 'pass' => $pass, 'ItemCode' => $product->sku);
-            $result = $client->call('GetPrice', $param);
-            $price = 0;
-            $promocion = false;
-            print_r($result);
-            echo '<br>';
-            if ($result == "" or $result == false) {
-                array_push($errors, json_encode(["id" => $product->id, "sku" => $product->sku]));
-                break;
-            }
-            if (!$result['GetPriceResult'] == "") {
-                $price = (float)$result['GetPriceResult']['Precios']['FinalPrice'];
-                $promocion = $result['GetPriceResult']['Precios']['promocion'] == 'Y' ? true : false;
-            } else {
-                array_push($errors, json_encode(["id" => $product->id, "sku" => $product->sku]));
-            }
-            $product->update(['price' => $price, 'producto_promocion' => $promocion]);
-        }
-        FailedJobsCron::create([
-            'name' => 'Promo Opcion',
-            'message' => "Productos No encontrados al actualizar el precio: " . implode(", ", $errors),
-            'status' => 0,
-            'type' =>   1
-        ]);
-        return $errors;
-    }
-
     public function getStockPromoOpcion()
     {
         $user = "DFE4516";
-        $xapikey = "ad3bdbcfd679bf6fd0b97b4b13809b22";
-        $headers = array(
-            "user: " . $user,
-            "x-api-key: " . $xapikey,
-        );
+        $passowrd = "5MrZtuzmiiuwSswLuONi";
+        $postFields = [
+            'user' => $user,
+            'password' => $passowrd,
+        ];
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_POST, 1);
-        // curl_setopt($ch, CURLOPT_POSTFIELDS, "demo=1"); //Opcional
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields); //Opcional
         curl_setopt(
             $ch,
             CURLOPT_URL,
-            "https://www.contenidopromo.com/wsds/mx/existencias/"
+            "https://promocionalesenlinea.net/api/all-stocks"
         );
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         $result = curl_exec($ch);
         curl_close($ch);
-        // Convertir en array
         $result = json_decode($result, true);
+        if (!isset($result['success'])) {
+            return $result;
+        }
+        if (!$result['success'] == true) {
+            return $result;
+        }
+
+        if (!isset($result['Stocks'])) {
+            return $result;
+        }
+        // Convertir en array
+        $stocks = $result['Stocks'];
+
         // return $response;
         $errors = [];
 
-        foreach ($result as $sku => $stock) {
-            $productCatalogo = Product::where('sku', $sku)->first();
+        foreach ($stocks as $stock) {
+            $productCatalogo = Product::where('sku', $stock['Material'])->first();
             if ($productCatalogo) {
-                $productCatalogo->update(['stock' => $stock]);
+                $productCatalogo->update(['stock' => $stock['Stock']]);
             } else {
-                array_push($errors, $sku);
+                array_push($errors, $stock['Material']);
             }
         }
+        return $errors;
 
         FailedJobsCron::create([
             'name' => 'Promo Opcion',
