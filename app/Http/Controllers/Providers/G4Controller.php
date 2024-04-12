5b792b541bf5d560e9ccf3e99dbb6bfa3ab71317
o0Fernando0o <119335001+o0Fernando0o@users.noreply.github.com>
2024-04-12 10:51:50 -0600

    Revert "ApiG4Renovado"

--- app/Http/Controllers/Providers/G4Controller.php
+++ app/Http/Controllers/Providers/G4Controller.php
@@ -34,9 +34,7 @@ class G4Controller extends Controller
         $response = $client->call('getProduct', $params);
         $products = (new SimpleXMLElement(base64_decode($response)));
         $productos = [];
-
-        foreach ($products->producto as $producto) {
-
+        foreach ($products as $producto) {
             $atributos = $producto->attributes();
             $data = [];
             $data['codigo_producto'] = (string) $atributos['codigo_producto'];
@@ -56,31 +54,11 @@ class G4Controller extends Controller
             $data['area_impresion'] = (string) $atributos['area_impresion'];
             $data['descriptores'] = (string) $atributos['descriptores'];
             $data['piezas_por_caja'] = (string) $atributos['piezas_por_caja'];
-            try {
-                $data['imagenes'] = (string) $producto->imagenes->principal->attributes()['url'];
-            } catch (Exception $e) {
-                $data['imagenes'] = 'default.jpg';
-            }
-            $precios = [];
-            for ($i = 0; $i < count($producto->precios->escala); $i++) {
-                $escala = $producto->precios->escala[$i]->attributes();
-                $precio = [
-                    'escala_inicial' => (string) $escala['rango'],
-                    'escala_final' => $i < count($producto->precios->escala) - 1
-                        ? (string)((int) $producto->precios->escala[$i + 1]['rango'] - 1)
-                        : '',
-                    'precio' => (string) $escala['precio']
-                ];
-                array_push($precios,  $precio);
-                
-            }
-            $data['precios'] = $precios;
-            if (count($data['precios']) > 0) {
-                array_push($productos, $data);
-            }
+            $data['precio'] = (string) $atributos['precio'];
             
+            array_push($productos, $data);
         }
-
+        dd($productos);
         $maxSKU = Product::max('internal_sku');
         $idSku = null;
         if (!$maxSKU) {
@@ -89,7 +67,7 @@ class G4Controller extends Controller
             $idSku = (int) explode('-', $maxSKU)[1];
             $idSku++;
         }
-
+     
         foreach ($productos as $product) {
             // Verificar si el color existe y si no registrarla
             $color = null;
@@ -118,27 +96,18 @@ class G4Controller extends Controller
                     'internal_sku' => "PROM-" . str_pad($idSku, 7, "0", STR_PAD_LEFT),
                     'sku' => $product['codigo_producto'],
                     'name' => $product['nombre_producto'],
-                    'price' => floatval($product['precios'][0]['precio']),
+                    'price' => floatval($product['precio']),
                     'description' => $product['descripcion'],
                     'stock' => 0,
                     'descuento' => 0,
                     'producto_promocion' => (int)$product['promocion'] == 0 ? false : true,
                     'producto_nuevo' => (int)$product['novedad'] == 0 ? false : true,
-                    'precio_unico' => false,
+                    'precio_unico' => true,
                     'provider_id' => 7,
                     'type_id' => 1,
                     'color_id' => $color->id,
                 ]);
-                $newProduct->images()->create(['image_url' =>  $product['imagenes']]);
-
-                // Registrar los precios
-                foreach ($product['precios'] as $precio) {
-                    $newProduct->precios()->create([
-                        'escala_inicial' => $precio['escala_inicial'],
-                        'escala_final' => $precio['escala_final'],
-                        'price' => floatval($precio['precio']), // Convertir a float antes de asignar
-                    ]);
-                }
+                $newProduct->images()->create(['image_url' =>  $product['imagen']]);
 
                 /*
                 Registrar en la tabla product_category el producto, categoria y sub categoria
@@ -195,18 +164,11 @@ class G4Controller extends Controller
                 }
                 $idSku++;
             } else {
-                $productExist->precios()->delete();
                 $productExist->update([
-                    "price" => floatval($product['precios'][0]['precio'])
+                    'price' => $product['precio'],
+                    'producto_promocion' => (int)$product['promocion'] == 0 ? false : true,
+                    'producto_nuevo' => (int)$product['novedad'] == 0 ? false : true,
                 ]);
-                // Registrar los precios
-                foreach ($product['precios'] as $precio) {
-                    $productExist->precios()->create([
-                        'escala_inicial' => $precio['escala_inicial'],
-                        'escala_final' => $precio['escala_final'],
-                        'price' => floatval($precio['precio']), // Convertir a float antes de asignar
-                    ]);
-                }
             }
         }
 
@@ -243,7 +205,6 @@ class G4Controller extends Controller
         DB::table('images')->where('image_url', '=', null)->delete();
     }
 
-
     public function getAllStockPL()
     {
         $wsdl = "https://distr.ws.g4mexico.com/index.php?wsdl";
@@ -260,9 +221,9 @@ class G4Controller extends Controller
         //llamada al método getProduct
         $response = $client->call('getProductStock', $params);
         $products = (new SimpleXMLElement(base64_decode($response)));
-        
+
         $productos = [];
-        foreach ($products->producto as $producto) {
+        foreach ($products as $producto) {
             $atributos = $producto->attributes();
             $data = [];
             $data['codigo_producto'] = (string) $atributos['codigo_producto'];
@@ -272,7 +233,7 @@ class G4Controller extends Controller
 
         $errors = [];
         foreach ($productos as $product) {
-            $productCatalogo = Product::where('provider_id', 7)->where('sku',  $product['codigo_producto'])->first();
+            $productCatalogo = Product::where('sku',  $product['codigo_producto'])->first();
             if ($productCatalogo) {
                 $productCatalogo->update(['stock' => (int)$product['existencias']]);
             } else {
@@ -288,8 +249,8 @@ class G4Controller extends Controller
                 if ($value->sku == $product['codigo_producto']) {
                     break;
                 }
-                unset($allProducts[$key]);
             }
+            unset($allProducts[$key]);
         }
         foreach ($allProducts as  $value) {
             $value->visible = 0;
