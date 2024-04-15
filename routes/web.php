0920a550ed8c84ef477796b1f02d470b5ffa3164
o0Fernando0o <2519360028fhernandezc@gmail.com>
2024-04-15 14:27:15 -0600

    Se modifico la API Intuicion ahora ya no se duplican los productos

--- routes/web.php
+++ routes/web.php
@@ -114,6 +114,5 @@ Route::get('/getAllProductsEuroCotton', [EuroCottonController::class, 'getAllPro
 // IntuicionPublicitaria
 Route::get('/getProductsIP', [IntuicionPublicitariaController::class, 'getProductsIP']);
 
-
 // Helpers
 Route::get('/changeProviderToInternalProducts', [HelperController::class, 'changeProviderToInternalProducts'])->name('companies');
